c2844a982e26c458ceb934b611b7edc3f6c50867
almostsurely <monsieurdozier@gmail.com>
2013-09-11 20:59:04 -0500

    Added support to ignore nsfw posts.

--- test.py
+++ test.py
@@ -7,8 +7,6 @@ import os
 def getFileType(link):
     file_type = link.split('.')[-1]
     file_type = file_type[:3]
-    if file_type == '.jpe':
-        file_type = '.jpeg'
     return file_type
 
 def downloadImage(link):
@@ -17,38 +15,35 @@ def downloadImage(link):
     except urllib.error.HTTPError as err:
         print(link + ' : ' + str(err.code) + ' : ' + err.reason)
         
-
+file_limit = 100
+nsfw = False
 file_types = ['jpg','gif','png']
 subreddits = ['funny','pics','AdviceAnimals']
+
+conn = http.client.HTTPConnection('www.reddit.com')
 
 for subreddit in subreddits:
 
     print('')
     print('Downloading ' + subreddit + ':')
-    
+
     if not os.path.exists('./pics/' + subreddit):
         os.makedirs('./pics/' + subreddit)
 
     hdr = {'User-Agent' : 'Testing some jazz'}
-    conn = http.client.HTTPConnection('www.reddit.com')
-    conn.request('GET', '/r/' + subreddit + '/.json?limit=100', headers=hdr)
+    conn.request('GET', '/r/' + subreddit + '/.json?limit=' + str(file_limit), headers=hdr)
     response = conn.getresponse().readall().decode('utf-8')
     json_dict = json.loads(response)
 
+    print(json_dict)
+
     for post in json_dict['data']['children']:
         pdata = post['data']
-        if pdata['is_self']:
+        link = pdata['url']
+        if pdata['is_self'] or pdata['over_18'] != nsfw:
             continue
         elif pdata['url'].split('.')[-1][:3] in file_types:
-            link = pdata['url']
             downloadImage(link)
         else:
-            print('Not Downloaded:' + pdata['domain'])
-        '''
-        elif pdata['domain'] == 'imgur.com':
-            #Replace with better imgur downloading, for example .gif's are wrong
-            link = pdata['url'] + '.jpg'
-            print(link)
-            urllib.request.urlretrieve(link, './pics/' + subreddit + '/' + pdata['id'] + '.' + getFileType(link))
-        '''
+            print('Not Downloaded: ' + link)
 conn.close()
